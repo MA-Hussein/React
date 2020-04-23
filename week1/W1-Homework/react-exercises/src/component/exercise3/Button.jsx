95e4dcad3ecd8ddb47b422456d850d740be0b71e
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2020-04-23 04:02:46 +0200

    Update W1-Exercises

--- week1/W1-Homework/react-exercises/src/component/exercise3/Button.jsx
+++ week1/W1-Homework/react-exercises/src/component/exercise3/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Button({ setCount }) {
-  return <button onClick={setCount}>Add 1!</button>;
+function Button({ onClick }) {
+  return <button onClick={onClick}>Add 1!</button>;
 }
 export default Button;
