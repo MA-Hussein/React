d0141586e9e7879c53cf6595a06654a4b3a3bd82
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2020-05-10 16:04:49 +0200

    Update WeatherCard.js

--- week3/HackYourFuture III/src/components/WeatherCard.js
+++ week3/HackYourFuture III/src/components/WeatherCard.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
 const WeatherCard = ({ props, deleteItem }) => {
+
+	const {id , name , sys: { country } , weather , main: { temp_min, temp_max } , coord: { lon, lat }} = props;
+
 	const kelvinToCelcius = (kelvin) => {
 		return (kelvin - 273).toFixed(2);
 	};
 	return (
 		<div className="main">
 			<h3>
-				{props.name} {props.sys.country}
+				{name} {country}
 			</h3>
-			<button className="deleteBtn" onClick={() => deleteItem(props.id)}>
+			<button className="deleteBtn" onClick={() => deleteItem(id)}>
 				X
 			</button>
 			<div className="weatherMain">
-				<h5>{props.weather[0].main}</h5>
-				<p> {props.weather[0].description} </p>
+				<h5>{weather[0].main}</h5>
+				<p> {weather[0].description} </p>
 			</div>
 
-			<p> min temp: {kelvinToCelcius(props.main.temp_min)} °C </p>
-			<p> max temp: {kelvinToCelcius(props.main.temp_max)} °C </p>
+			<p> min temp: {kelvinToCelcius(temp_min)} °C </p>
+			<p> max temp: {kelvinToCelcius(temp_max)} °C </p>
 			<p>
-				location: {props.coord.lat} {props.coord.lon}
+				location: {lat} {lon}
 			</p>
 		</div>
 	);
